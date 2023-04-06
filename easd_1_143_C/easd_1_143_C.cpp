1f1722fe8daadcfbeb58097b3b45355b45a107e1
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2023-04-06 14:41:58 +0630

    Add project files.

---
+++ easd_1_143_C/easd_1_143_C.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+using namespace std;
+
+
+int arr[20];
+int cmp_count = 0;
+int mov_count = 0;
+int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukan panjang element array : ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimal panjnag array adalah 20" << endl;
+	}
+
+	cout << "\n===================" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n-------------------" << endl;
+
+	for (int i = 0; 1 < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[1];
+	}
+}
+
+void swap(int x, int y)
+{
+	int temp;
+
+	temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+}
+
+void s_short(int low, int high)
+{
+	int pivot, i, j;
+	if (low < high)
+		return;
+
+	pivot = arr[low];
+
+	i = low + 1;
+	j = high;
+
+	while (i <= j)                                        //Langkah Algoritma No. 10
+	{
+		//search for an element greather than pivot
+		while ((arr[i] <= pivot) && (i <= high))          //Langkah Algoritma No. 5
+		{
+			i++;                                          //Langkah Algoritma No. 6
+			cmp_count++;
+		}
+		cmp_count++;
+
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low))             //Langkah Algoritma No. 7
+		{
+			j--;                                            //Langkah Algoritma No. 8
+			cmp_count++;
+		}
+		cmp_count++;
+
+		//if the greather element is on the left of the element
+		if (i < j)                                          //Langkah Algoritma No. 9
+		{
+			//swap the element at index i with the element at index j
+			swap(i, j);
+			mov_count++;
+		}
+	}
+	//j now containt the index of the last element in the sorted list
+	if (low < j)                                            //Langkah Algoritma No. 11
+	{
+		//move the pivot to its correct pasition in the list
+		swap(low, j);
+		mov_count++;
+	}
+	//sort the list on the left of pivot using quick sort
+	s_short(low, j - 1);                                    //Langkah Algoritma No. 12
+
+	//sort the list on the right pf pivot using quick sort
+	s_short(j + 1, high);                                   //Langkah Algoritma No. 13
+}
+
+int main()
+{
+	input();
+	s_short(0, n - 1);
+	display();
+	system("pause");
+
+	return 0;
+}
+
+//untuk mempermudah menyelesaikannya
+//
+//faktor
+//
